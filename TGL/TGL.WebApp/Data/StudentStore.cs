f7820cea984dd0afd85e1224acd667fea4d5ec30
vivelab8 <vivelab8@DESKTOP-HLTHUM7>
2019-10-19 13:01:40 -0500

    Delete

---
+++ TGL/TGL.WebApp/Data/StudentStore.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TGL.WebApp.Models;
+
+namespace TGL.WebApp.Data
+{
+    public class StudentStore
+    {
+        private List<Student> Students { get; set; } = new List<Student>();
+        public StudentStore()
+        {
+            Students.Add(new Student
+            {
+                Id=Guid.NewGuid(),
+                Age = 21,
+                Name = "Sergio",
+                LastName = "Garcia",
+                Nit = "0001"
+            });
+            Students.Add(new Student
+            {
+                Id = Guid.NewGuid(),
+                Age = 19,
+                Name = "Juan",
+                LastName = "Perez",
+                Nit = "0002"
+            });
+            Students.Add(new Student
+            {
+                Id = Guid.NewGuid(),
+                Age = 23,
+                Name = "Andres",
+                LastName = "Lopez",
+                Nit = "0003"
+            });
+            Students.Add(new Student
+            {
+                Id = Guid.NewGuid(),
+                Age = 18,
+                Name = "Laura",
+                LastName = "Gaviria",
+                Nit = "0004"
+            });
+        }
+
+        internal void DeleteStudent(Guid id)
+        {
+            var student = Students.FirstOrDefault(x => x.Id == id);
+            Students.Remove(student);
+        }
+
+        public List<Student> GetStudents()
+        {
+            return this.Students;
+        }
+
+    }
+}
--- TGL/TGL.WebApp/Startup.cs
+++ TGL/TGL.WebApp/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using TGL.WebApp.Data;
 
 namespace TGL.WebApp
 {
@@ -24,6 +25,9 @@ namespace TGL.WebApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+
+
+            services.AddSingleton<StudentStore>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
